52d9b72489fc3ebb4e9e029b8349e4d11c1a5d0d
masanao.matsubara <m_matsubara@okwave.co.jp>
2020-02-03 23:02:10 +0900

    caliculate

--- caluculate.js
+++ caluculate.js
@@ -2,58 +2,63 @@
  * 投票結果の集計
  */
 function caliculate() {
-  console.log("test");
-  //Browser.msgBox("test");
   var sheet=SpreadsheetApp.getActiveSheet();
 
   // 結果の保存
   var result = {};
-
-  var cells = sheet.getRange(2,3,2,13);
-  for(var i=0; i < 5; i+=1){
-    var workName = cells.getCell(1,i*2 + 1).getValue();
-    var creatorName = cells.getCell(1,i*2 + 2).getValue();
-    if( workName=="" ) break;
-
-    Browser.msgBox(workName);
-
-    if(!result[[workName, creatorName]]){
-      result[[workName, creatorName]] = 0;
-    }
-    result[[workName, creatorName]] += 1;
-  }
-  
-  for(var item in result){
-    //Browser.msgBox(item);
-    //Browser.msgBox(typeof item);
-    
-    if(item.indexOf(",") != -1){
-      Browser.msgBox([item,result[item]]);
-    }
-  }
-
-  //Browser.msgBox(result);
-  
-  //Browser.msgBox(result);
+  result = caliculateRows(sheet, result);
+  var arr = hashToTable(result);
+  arr.sort(function(a,b){
+    if(a[2] == b[2]) return 0;
+    if(a[2] < b[2]) return 1;
+    return -1;
+  });  
+  Browser.msgBox(arr);
 }
 
-
-function caliculateRow(sheet, rowNum, result){
-  
-  var cells = sheet.getRange(rowNum,3,rowNum,13);
-
-  for(var i=0; i < 5; i+=1){
-    var workName = cells.getCell(1,i*2 + 1).getValue();
-    var creatorName = cells.getCell(1,i*2 + 2).getValue();
-    if( workName=="" ) break;
-
-    Browser.msgBox(workName);
-
-    if(!result[[workName, creatorName]]){
-      result[[workName, creatorName]] = 0;
+function hashToTable(result){
+  var arr = [];
+  for(var creatorName in result){
+    var creator = result[creatorName];    
+    for(var workName in creator){
+      if(!creator[workName]) continue;
+      
+      arr.push([creatorName,workName,creator[workName]]);
+      //Browser.msgBox([creatorName,workName,creator[workName]]);      
     }
-    result[[workName, creatorName]] += 1;
   }
   
+  return arr;
+}
+
+function caliculateRows(sheet, result){
+  var rowFlg = true;
+  for(var i=2; rowFlg; i++){
+    var o = caliculateRow(sheet, i, result);
+    rowFlg = o.flg;
+    result = o.result;
+  }
   return result;
+}
+
+function caliculateRow(sheet, rowNum, result){
+  var cells = sheet.getRange(rowNum,3,rowNum,13);
+  for(var i=0; i < 5; i+=1){
+    var workName = cells.getCell(1, i*2 + 1).getValue();
+    var creatorName = cells.getCell(1, i*2 + 2).getValue();
+    if( workName=="" && i==0 ) return { result: result, flg: false };
+    if( workName=="" ) break;
+
+    //Browser.msgBox(workName);
+    
+    if(!result[creatorName]){
+      result[creatorName] = {};
+    }
+    if(!result[creatorName][workName]){
+      result[creatorName][workName] = 0;
+    }
+    result[creatorName][workName] += 1;
+  }
+  
+  return { result: result, flg: true };
 }
